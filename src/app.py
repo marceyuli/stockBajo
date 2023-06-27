b39eaaa29c685d4a513dd59998eae68c1dea76c0
marceyuli <yulianamarcela200@gmail.com>
2023-06-27 15:27:11 -0400

    Correos se envian correctamente

--- src/app.py
+++ src/app.py
@@ -1,8 +1,43 @@
 from flask import Flask
 import requests
 import json
-
+import smtplib 
+from email.mime.text import MIMEText
 app = Flask(__name__)
+
+def enviar_notificacion_por_correo(producto_nombre):
+    # Configura los detalles del servidor de correo y las credenciales de autenticación
+    servidor_smtp = "smtp-relay.sendinblue.com"
+    puerto_smtp = 587
+    usuario_smtp = "yulianamarcela200@gmail.com"
+    contrasena_smtp = "rFAjnUHaVDXZzMI6"
+
+    remitente = "bambamAdmin@gmail.com"
+    destinatario = "yulianamarcela200@gmail.com"
+    asunto = "Stock bajo de producto"
+    mensaje = f"El stock del producto {producto_nombre} está por debajo del stock minimo, tomar previsiones. "
+
+    # Crea el objeto MIMEText con el contenido del mensaje
+    mensaje_mime = MIMEText(mensaje)
+    mensaje_mime["From"] = remitente
+    mensaje_mime["To"] = destinatario
+    mensaje_mime["Subject"] = asunto
+
+    try:
+        # Conecta y autentica con el servidor SMTP
+        servidor = smtplib.SMTP(servidor_smtp, puerto_smtp)
+        servidor.starttls()
+        servidor.login(usuario_smtp, contrasena_smtp)
+
+        # Envía el correo electrónico
+        servidor.send_message(mensaje_mime)
+
+        # Cierra la conexión con el servidor SMTP
+        servidor.quit()
+
+        print("Notificación enviada por correo electrónico")
+    except Exception as e:
+        print("Error al enviar la notificación por correo electrónico:", str(e))
 
 def obtener_datos_productos():
     url_endpoint = "http://177.222.127.82/bambamws/api/productos/obtenerproductos"  # Reemplaza con la URL real del endpoint
@@ -22,12 +57,13 @@ def notificarStockBajo():
             
             if proStock < umbral:
                 print(f"El stock del producto {producto['proNombre']} está por debajo del umbral.")
+                enviar_notificacion_por_correo(producto['proNombre'])
                 # Aquí puedes agregar la lógica para enviar la notificación, como enviar un correo electrónico o llamar a una función específica.
 
 @app.route('/')
 def hello():
     notificarStockBajo()
-    return '¡Hola, Flask!'
+    return 'Microservicio de notificaciones Bambam'
 
 if __name__ == '__main__':
     app.run()
